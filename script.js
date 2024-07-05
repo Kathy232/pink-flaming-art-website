f5a4fcceb062f416e914514b959f4925803b9a32
Kasia <164321219+Kathy232@users.noreply.github.com>
2024-07-05 12:53:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Przykładowa funkcja JavaScript do zmiany koloru tła elementu po kliknięciu
+function zmienKolorTla() {
+    document.getElementById('moj-element').style.backgroundColor = 'lightblue';
+}
+
+// Przykładowa funkcja JavaScript do wyświetlenia alertu po załadowaniu strony
+window.onload = function() {
+    alert('Witaj na mojej stronie!');
+}
+
